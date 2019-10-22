57b0848a45c8f54f522954b6d297b7b3d6f69129
yeongjet <625436194@163.com>
2019-10-22 20:13:39 +0800

    update

---
+++ packages/goods/src/api/goods/query-goods.effect.ts
@@ -0,0 +1,57 @@
+import { HttpError, HttpStatus, EffectFactory, use } from '@marblejs/core'
+import { requestValidator$, t } from '@marblejs/middleware-io'
+import { throwError, of, from } from 'rxjs'
+import { mergeMap, map, catchError } from 'rxjs/operators'
+import { getRepository } from 'typeorm'
+import { neverNullable } from '@mazongguan-common/filter'
+import { GoodsModel } from '../../model'
+
+const validator$ = requestValidator$({
+    body: t.type({
+        enterprise_id: t.number,
+        batch_name: t.string,
+        code_count: t.number,
+        loss_rate: t.number,
+        label_type: t.number,
+        product_id: t.number,
+        extra_info: t.type({
+            export_column: t.type({
+                inside_code: t.type({
+                    code_type: t.number
+                })
+            })
+        })
+    })
+})
+
+export const createBatch$ = EffectFactory.matchPath('/')
+    .matchType('POST')
+    .use(req$ =>
+        req$.pipe(
+            use(validator$),
+            mergeMap(req =>
+                of(req.body).pipe(
+                    mergeMap((goods: GoodsModel) =>
+                        from(getRepository(GoodsModel).save(goods))
+                    ),
+                    mergeMap(neverNullable),
+                    map(batch => ({
+                        body: {
+                            code: 10000,
+                            data: {
+                                batch: batch
+                            }
+                        }
+                    })),
+                    catchError(error =>
+                        throwError(
+                            new HttpError(
+                                `Consumer create fail: ${error}`,
+                                HttpStatus.INTERNAL_SERVER_ERROR
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    )
